14388c9063c2a645a18b121a41a4cc4b60058d30
Robert Brower <robertbrower.technologies@gmail.com>
2017-04-23 15:56:49 +0200

    modified:   package.json 	modified:   src/package.json 	modified:   src/rb-dashboard.component.html 	modified:   src/rb-dashboard.component.ts

--- src/rb-dashboard.component.ts
+++ src/rb-dashboard.component.ts
@@ -7,8 +7,12 @@ import { RbListService } from 'rb-list-service';
   templateUrl: './rb-dashboard.component.html'
 })
 export class RbDashboardComponent {
-
+  value: number;
   constructor(private rbListService: RbListService) {
   }
-
+  getValue(gender) {
+    var len = this.rbListService.items.length;
+    var filteredItems = this.rbListService.items.filter((item) => item.gender === gender);
+    return (filteredItems.length / len) * 100;
+  }
 }
